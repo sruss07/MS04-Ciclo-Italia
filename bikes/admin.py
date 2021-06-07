3afda8faada5f528109d709084833c42266a4677
sruss07 <srussell1903@yahoo.co.uk>
2021-06-07 14:48:43 +0000

    deleted code which was preventing loaddata to perform correctly

--- bikes/admin.py
+++ bikes/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 from .models import Bike, Brand
 
 
-admin.site.register(Bike, BikeAdmin)
-admin.site.register(Brand, BrandAdmin)
-
-
+admin.site.register(Bike)
+admin.site.register(Brand)
