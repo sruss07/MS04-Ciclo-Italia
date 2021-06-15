6fe99ebf3bebee738671619c1f7a8aa7513d37ad
sruss07 <srussell1903@yahoo.co.uk>
2021-06-15 10:08:07 +0000

    updated order form fields

--- checkout/models.py
+++ checkout/models.py
@@ -1,42 +1,58 @@
 import uuid
 
-from django.db.models import Sum
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from django_countries.fields import CountryField
 
 from bikes.models import Bike
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
-
-    order_number = models.CharField(max_length=40, null=False, editable=False)
-    first_name = models.CharField(max_length=20, null=False, blank=False)
-    last_name = models.CharField(max_length=20, null=False, blank=False)
-    email_address = models.CharField(max_length=50, null=False, blank=False)
-    address = models.CharField(max_length=100, null=False, blank=False)
-    town = models.CharField(max_length=50, null=False, blank=False)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    order_date = models.DateField("Order Date", auto_now_add=True)
-    cart_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(
+        max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    county = models.CharField(
+        max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        null=False, default=0)
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
-        Generates a unique order number. Code from CI checkout lesson
+        Creates random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
-    def update_cart_total(self):
-        """Updates cart total each time an item is added.
-        Code from CI checkout lesson"""
-        self.cart_total = self.lineitems.aggregate(
-                            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+    def update_total(self):
+        """
+        Update grand total each time an item is added
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.save()
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method and set the order number
-        if not set already. Code from CI checkout lesson
+        Override original save method to set the order number
+        if it hasn't been set already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
@@ -48,19 +64,20 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
-                            Order, null=False, blank=False,
-                            on_delete=models.CASCADE, related_name='lineitems')
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems')
     bike = models.ForeignKey(
-                        Bike, null=False, blank=False, on_delete=models.CASCADE)
+        Bike, null=False,
+        blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
-                                max_digits=6, decimal_places=2, null=False,
-                                blank=False, editable=False)
+        max_digits=6, decimal_places=2,
+        null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
-        if it hasn't been set already. Code used from CI checkout lesson
+        Override the original save method to set the lineitem total
+        and update the order total.
         """
         self.lineitem_total = self.bike.price * self.quantity
         super().save(*args, **kwargs)
