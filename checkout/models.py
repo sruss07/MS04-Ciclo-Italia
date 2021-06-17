f826859863cdf8a462fa9bf0d50f1823a871894a
sruss07 <srussell1903@yahoo.co.uk>
2021-06-17 10:23:12 +0000

    added import datetime

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,5 @@
 import uuid
+import datetime
 
 from django.db import models
 from django.db.models import Sum
@@ -28,7 +29,7 @@ class Order(models.Model):
         max_length=80, null=True, blank=True)
     county = models.CharField(
         max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True, default=datetime.date.today)
     order_total = models.DecimalField(
         max_digits=10, decimal_places=2,
         null=False, default=0)
