cfeb7de89a5beecb3f799a22d7eff3069f2d9f13
sruss07 <srussell1903@yahoo.co.uk>
2021-06-20 10:11:07 +0000

    updated add to cart code to fix problem adding products

--- cart/views.py
+++ cart/views.py
@@ -12,20 +12,20 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 
-def add_to_cart(request, item_id):
+def add_to_cart(request, bike_id):
     """ Add a specified quantity of a bike to shopping cart """
 
-    bike = get_object_or_404(Bike, pk=item_id)
+    bike = get_object_or_404(Bike, pk=bike_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if bike_id in list(cart.keys()):
+        cart[bike_id] += quantity
         messages.success(
-            request, f'Updated {bike.name} quantity to {cart[item_id]}')
+            request, f'Updated {bike.name} quantity to {cart[bike_id]}')
     else:
-        cart[item_id] = quantity
+        cart[bike_id] = quantity
         messages.success(request, f'You have added {bike.name} to your cart')
 
     request.session['cart'] = cart
@@ -33,38 +33,38 @@ def add_to_cart(request, item_id):
     return redirect(redirect_url)
 
 
-def update_cart(request, item_id):
+def update_cart(request, bike_id):
     """Adjust quantity of specified bike"""
 
-    bike = get_object_or_404(Bike, pk=item_id)
+    bike = get_object_or_404(Bike, pk=bike_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
-        cart[item_id] = quantity
+        cart[bike_id] = quantity
         messages.success(
-            request, f'Updated {bike.name} quantity to {cart[item_id]}')
+            request, f'Updated {bike.name} quantity to {cart[bike_id]}')
     else:
-        cart.pop(item_id)
+        cart.pop(bike_id)
         messages.success(request, f'Removed {bike.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
 
-def remove_from_cart(request, item_id):
+def remove_from_cart(request, bike_id):
     """Remove bike from shopping cart"""
 
     try:
-        bike = get_object_or_404(Bike, pk=item_id)
+        bike = get_object_or_404(Bike, pk=bike_id)
         cart = request.session.get('cart', {})
 
-        cart.pop(item_id)
+        cart.pop(bike_id)
         messages.success(request, f'Removed {bike.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
+        messages.error(request, f'Error removing bike: {e}')
         return HttpResponse(status=500)
