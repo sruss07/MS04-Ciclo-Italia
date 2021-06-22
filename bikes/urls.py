66bb5ab5e5264ecabac91fc7a2bd128caae0e80b
sruss07 <srussell1903@yahoo.co.uk>
2021-06-22 16:20:03 +0000

    removed erroneous url path

--- bikes/urls.py
+++ bikes/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('<int:bike_id>/', views.bike_detail, name='bike_detail'),
     path('add/', views.add_bike, name='add_bike'),
     path('edit/<int:bike_id>/', views.edit_bike, name='edit_bike'),
-    path(
-        'delete/<int:bike_id>/',
-        views.delete_bike, name='delete_bike'),
+    path('delete/<int:bike_id>/', views.delete_bike, name='delete_bike'),
 ]
