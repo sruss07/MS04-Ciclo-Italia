b81aaa1ced2ee149ba8eb752d260c6e6a3699588
sruss07 <srussell1903@yahoo.co.uk>
2021-06-25 08:01:54 +0000

    removed x-frame code for creating mockup

--- ciclo_italia/settings.py
+++ ciclo_italia/settings.py
@@ -71,8 +71,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
-
 ROOT_URLCONF = 'ciclo_italia.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
